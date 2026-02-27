ed09f2d5c768eceb024ad1ea3b276843355160de
kennedy-76 <kennedykirui76@gmail.com>
2026-02-27 16:59:56 +0300

    admin_routes.py

--- app/routes/admin_routes.py
+++ app/routes/admin_routes.py
@@ -86,11 +86,10 @@ def add_movie():
             trailer_filename = secure_filename(trailer_file.filename) if trailer_file else None
 
             # Upload movie to Cloudinary
-            movie_upload = cloudinary.uploader.upload_large(
+            movie_upload = cloudinary.uploader.upload(
                 movie_file,
                 resource_type="video",
-                folder="movies",
-                chunk_size=6000000  # 6MB chunks
+                folder="movies"
             )
             movie_filename = movie_upload["secure_url"]
 
