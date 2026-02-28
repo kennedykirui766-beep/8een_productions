61a2e97078f0b96d57d0f3561f0face306c162c5
kennedy-76 <kennedykirui76@gmail.com>
2026-02-28 15:29:28 +0300

    config.py

--- config.py
+++ config.py
@@ -15,7 +15,7 @@ class Config:
     MPESA_PASSKEY = os.getenv("MPESA_PASSKEY")
     MPESA_CALLBACK_URL = os.getenv("MPESA_CALLBACK_URL")
     SECRET_KEY = "your-secret-key"
-    LOGO_URL = "https://res.cloudinary.com/dvctsb0sm/image/upload/v1772266155/logo.png_yicnh1.jpg"
+    LOGO_URL = "https://res.cloudinary.com/dvctsb0sm/image/upload/v1772266155/logo.png_isqnga.jpg"
     ABOUT_IMAGE_URL = "https://res.cloudinary.com/dvctsb0sm/image/upload/v1772266168/about.jpg_yicnh1.jpg"
     
     
