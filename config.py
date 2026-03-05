003048dcfedc14390e484686afd345cb0e032cd1
chipukizii <koechgilton@gmail.com>
2026-03-05 22:57:27 +0300

    config.py

--- config.py
+++ config.py
@@ -27,7 +27,7 @@ class Config:
     CLIENT_VIDEO_1 = "https://res.cloudinary.com/dvctsb0sm/video/upload/v1772727536/32945-395456395_medium_axo2ca.mp4"
     CLIENT_VIDEO_2 = "https://res.cloudinary.com/dvctsb0sm/video/upload/v1772728217/35264-407130725_medium_by66te.mp4"
 
-    CLIENT_VIDEO_THUMB_1 = "https://res.cloudinary.com/dvctsb0sm/image/upload/v1772728224/3652586-smile-2072907_1920_fdhupt.jpg1"
+    CLIENT_VIDEO_THUMB_1 = "https://res.cloudinary.com/dvctsb0sm/image/upload/v1772728224/3652586-smile-2072907_1920_fdhupt.jpg"
     CLIENT_VIDEO_THUMB_2 = "https://res.cloudinary.com/dvctsb0sm/image/upload/v1772728222/susanne906-portrait-5115894_1920_ganm9l.jpg"
 
 
