a5786c0e188eae68ed3480ea444119bacaebe381
chipukizii <koechgilton@gmail.com>
2026-03-09 09:03:16 +0300

    user.py

--- app/models/user.py
+++ app/models/user.py
@@ -4,24 +4,15 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from itsdangerous import URLSafeTimedSerializer
 from flask import current_app
 
-from app import db
-from flask_login import UserMixin
-from werkzeug.security import generate_password_hash, check_password_hash
-from itsdangerous import URLSafeTimedSerializer
-from flask import current_app
-
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
 
     username = db.Column(db.String(100), unique=True, nullable=False)
-
     email = db.Column(db.String(120), unique=True, nullable=False)
-
     password_hash = db.Column(db.String(255), nullable=False)
 
     is_paid = db.Column(db.Boolean, default=False)
-
     is_admin = db.Column(db.Boolean, default=False)
 
     price = db.Column(db.Float, nullable=True)
@@ -33,16 +24,20 @@ class User(db.Model, UserMixin):
         return check_password_hash(self.password_hash, password)
 
     def get_reset_token(self):
-        s = URLSafeTimedSerializer(current_app.config['SECRET_KEY'])
-        return s.dumps(self.id, salt='password-reset-salt')
+        serializer = URLSafeTimedSerializer(current_app.config["SECRET_KEY"])
+        return serializer.dumps(self.id, salt="password-reset-salt")
 
     @staticmethod
     def verify_reset_token(token, expiration=3600):
-        s = URLSafeTimedSerializer(current_app.config['SECRET_KEY'])
+        serializer = URLSafeTimedSerializer(current_app.config["SECRET_KEY"])
 
         try:
-            user_id = s.loads(token, salt='password-reset-salt', max_age=expiration)
+            user_id = serializer.loads(
+                token,
+                salt="password-reset-salt",
+                max_age=expiration
+            )
         except Exception:
             return None
 
-        return User.query.get(user_id)
+        return db.session.get(User, user_id)
